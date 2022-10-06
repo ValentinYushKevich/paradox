dc56b9e8182b777cbcbe17fb857132729e956ac8
Valentin Yushkevich <v.yushkevich@bsl.dev>
2022-10-06 09:59:27 +0300

    add search

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,22 @@
   <header class="header">
     <h2 class="header__title">Документы</h2>
     <div class="header__buttons">
-      <ButtonFlag />
+      <ButtonFlag/>
 
-      <ButtonComponent v-for="button in buttons" :title="button.title" :key="button.id" />
+      <ButtonComponent v-for="button in buttons" :title="button.title" :key="button.title"/>
     </div>
   </header>
+  <div class="search_block">
+    <InputSearch v-model="searchKey" />
+  </div>
 </template>
 
 <script setup>
-import ButtonFlag from './components/ButtonFlag.vue';
+import { ref } from 'vue'
 import ButtonComponent from './components/ButtonComponent.vue';
+import ButtonFlag from './components/ButtonFlag.vue';
+import InputSearch from './components/InputSearch.vue'
 
+const searchKey = ref('')
 const buttons = [{ id: 1, title: 'Новый тип' }, { id: 2, title: 'Новый документ' },]
 </script>
